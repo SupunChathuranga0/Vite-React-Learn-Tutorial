7f4c9ce98bce1d1a518b20369aa3baa1426fa92b
Supun Chathuranga <104726557+SupunChathuranga0@users.noreply.github.com>
2024-11-07 21:56:14 +0530

    Photo add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 import BodyContent from './Components/BodyContent/BodyContent';
-
 import HeaderContent from './Components/HeaderContent/HeaderContent';
+import logo from '.././src/assets/react.svg'
 
 function App() {
-
+   
   return (
     <>
     <div className="wrapper">
@@ -17,6 +17,13 @@ function App() {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam molestias sequi distinctio quae itaque accusamus, voluptatibus fugiat, ea quidem libero alias nulla vitae, nihil aspernatur esse voluptas sunt? Nostrum.</p>
         <br/>
         <a href="http://www.google.com" >Google</a>
+        <br />
+        <img id='svg' src="vite.svg" alt="" />
+        <br />
+        <img className='reactlogo' src={logo} alt="react logo" /> 
+        </div>
+        <div>
+
         </div>
 
       </BodyContent>
--- src/Components/BodyContent/BodyContent.jsx
+++ src/Components/BodyContent/BodyContent.jsx
@@ -6,11 +6,12 @@ function BodyContent(props) {
     // <div id='bodycontent'>
     //     <p>main content</p>
     // </div>
-
+<>
     <div className='content'>
       <h3>Main Content</h3>
       {props.children}
     </div>
+    </>
   )
 }
 
